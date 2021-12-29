e4c0c41d0e43f0e196a5db2dfa160b639ea29893
FrontEnd <C:\Users\Work\AppData\Roaming\The Bat!\Google>
2021-12-29 04:53:55 +0300

    Исправляет крестик на попапе логина

--- build/js/main.js
+++ build/js/main.js
@@ -132,216 +132,6 @@
 
 
 //Управление слайдером-свайпером.
-/* (function () {
-  const styleSwiperList = document.querySelector('.js__styles-swiper-list--no-js');
-  const styleSwiperItem = document.querySelectorAll('.js__styles-swiper-item--no-js');
-  const styleSwiperButton = document.querySelectorAll('.js__styles-swiper-button--no-js');
-
-  if (styleSwiperList && styleSwiperItem && styleSwiperButton) {
-    styleSwiperList.classList.remove('js__styles-swiper-list--no-js');
-
-    styleSwiperItem.forEach((element) => {
-      element.classList.remove('js__styles-swiper-item--no-js');
-    });
-
-    styleSwiperButton.forEach((element) => {
-      element.classList.remove('js__styles-swiper-button--no-js');
-    });
-
-    const SWIPER_DESKTOP = {
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return `<span class="${className}">${index + 1}</span>`;
-        },
-      },
-
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      simulateTouch: false,
-      slidesPerView: 4,
-      slidesPerGroup: 4,
-      spaceBetween: 30,
-    };
-
-    const SWIPE_TABLET = {
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return `<span class="${className}">${index + 1}</span>`;
-        },
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      simulateTouch: true,
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 30,
-    };
-
-    const SWIPER_MOBILE = {
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'fraction',
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      simulateTouch: true,
-      slidesPerView: 2,
-      slidesPerGroup: 2,
-      spaceBetween: 30,
-    };
-
-    const swiperArea = document.querySelector('.swiper');
-    const DESKTOP_WIDTH = 1023;
-    const desktopNoTouchValue = window.screen.width <= DESKTOP_WIDTH;
-
-    const restartSwiper = (touch) => {
-      //Ниже строка не проверяется линтером - это вызов библиотеки слайдера-свайпера
-      // eslint-disable-next-line no-undef
-      new Swiper(swiperArea, {
-        simulateTouch: touch,
-        loop: false,
-        breakpoints: {
-          1023: SWIPER_DESKTOP,
-          767: SWIPE_TABLET,
-          0: SWIPER_MOBILE,
-        },
-      });
-    };
-
-    restartSwiper(desktopNoTouchValue);
-
-    window.addEventListener('resize', () => {
-      if (window.screen.width <= DESKTOP_WIDTH) {
-        setTimeout(() => restartSwiper(true), 500);
-      }
-      if (window.screen.width > DESKTOP_WIDTH) {
-        setTimeout(() => restartSwiper(false), 500);
-      }
-    });
-  }
-}());
- */
-//Управление аккордеоном секции Filters.
-(function () {
-
-  const filtersRest = document.querySelectorAll('.js__filters--start-hidden');
-  const filtersAll = document.querySelectorAll('.js__filters');
-
-  if (filtersRest && filtersAll) {
-
-    filtersRest.forEach((filter) => {
-      filter.classList.add('js__filters--hidden');
-    });
-
-    filtersAll.forEach((filter) => {
-      const answer = filter.querySelector('h3');
-      answer.addEventListener('click', () => {
-        filter.classList.toggle('js__filters--hidden');
-      });
-
-      answer.addEventListener('keydown', (evt) => {
-        if (evt.key === 'Enter') {
-          filter.classList.toggle('js__filters--hidden');
-        }
-      });
-    });
-  }
-
-}());
-
-//Переключение чекбоксов секции Filters с помощью клавиши Enter при управлении с клавиатуры.
-(function () {
-  const areaInputs = document.querySelectorAll('.filter__input-custom');
-
-  if (areaInputs) {
-    areaInputs.forEach((area) => {
-      area.addEventListener('keydown', (evt) => {
-        if (evt.keyCode === 32 || evt.key === 'Enter') {
-          evt.preventDefault();
-          const enter = area.querySelector('input');
-          enter.toggleAttribute('checked');
-        }
-      });
-    });
-  }
-}());
-
-//Управление попапом фильтра страницы catalog.html
-(function () {
-  const filter = document.querySelector('.js__filter');
-  const buttonOpen = document.querySelector('.js__form-button-open');
-  const buttonClosed = document.querySelector('.js__form-button-closed');
-  const blockHiddenFilter = document.querySelectorAll('.js__block-hidden-filter');
-  const body = document.querySelector('.page-body');
-  const backgroundPopup = document.querySelector('.js__filter-background');
-
-  if (buttonOpen && filter && buttonClosed && blockHiddenFilter && body && backgroundPopup) {
-    filter.classList.add('js__filter--hidden');
-    buttonOpen.classList.add('js__form-button-open--active');
-
-    buttonOpen.addEventListener('click', () => {
-      filter.classList.remove('js__filter--hidden');
-      filter.classList.add('js__filter--popup');
-      buttonOpen.classList.remove('js__form-button-open--active');
-      buttonClosed.classList.add('js__form-button-closed--active');
-      body.classList.add('js__body--no-scroll');
-      backgroundPopup.classList.add('js__filter-background--popup');
-
-      blockHiddenFilter.forEach((block) => {
-        block.classList.add('js__block-hidden--active');
-      });
-    });
-
-    const deactivePopupFilter = () => {
-      filter.classList.add('js__filter--hidden');
-      filter.classList.remove('js__filter--popup');
-      buttonOpen.classList.add('js__form-button-open--active');
-      buttonClosed.classList.remove('js__form-button-closed--active');
-      body.classList.remove('js__body--no-scroll');
-      backgroundPopup.classList.remove('js__filter-background--popup');
-
-      blockHiddenFilter.forEach((block) => {
-        block.classList.remove('js__block-hidden--active');
-      });
-    };
-
-    buttonClosed.addEventListener('click', () => {
-      deactivePopupFilter();
-    });
-
-    if (screen.width <= 1023) {
-      filter.addEventListener('submit', (evt) => {
-        evt.preventDefault();
-        deactivePopupFilter();
-      });
-    }
-
-    document.addEventListener('keydown', (evt) => {
-      if (evt.key === 'Escape') {
-        deactivePopupFilter();
-      }
-    });
-
-    document.addEventListener('click', (evt) => {
-      if (evt.target === backgroundPopup) {
-        deactivePopupFilter();
-      }
-    });
-  }
-}());
-
 (function () {
   const styleSwiperList = document.querySelector('.js__styles-swiper-list--no-js');
   const styleSwiperItem = document.querySelectorAll('.js__styles-swiper-item--no-js');
@@ -429,6 +219,115 @@
     swiper.on('breakpoint', () => {
       swiper.pagination.render();
       swiper.pagination.update();
+    });
+  }
+}());
+
+//Управление аккордеоном секции Filters.
+(function () {
+
+  const filtersRest = document.querySelectorAll('.js__filters--start-hidden');
+  const filtersAll = document.querySelectorAll('.js__filters');
+
+  if (filtersRest && filtersAll) {
+
+    filtersRest.forEach((filter) => {
+      filter.classList.add('js__filters--hidden');
+    });
+
+    filtersAll.forEach((filter) => {
+      const answer = filter.querySelector('h3');
+      answer.addEventListener('click', () => {
+        filter.classList.toggle('js__filters--hidden');
+      });
+
+      answer.addEventListener('keydown', (evt) => {
+        if (evt.key === 'Enter') {
+          filter.classList.toggle('js__filters--hidden');
+        }
+      });
+    });
+  }
+
+}());
+
+//Переключение чекбоксов секции Filters с помощью клавиш Enter и Пробел при управлении с клавиатуры.
+(function () {
+  const areaInputs = document.querySelectorAll('.filter__input-custom');
+
+  if (areaInputs) {
+    areaInputs.forEach((area) => {
+      area.addEventListener('keydown', (evt) => {
+        if (evt.keyCode === 32 || evt.key === 'Enter') {
+          evt.preventDefault();
+          const enter = area.querySelector('input');
+          enter.toggleAttribute('checked');
+        }
+      });
+    });
+  }
+}());
+
+//Управление попапом фильтра страницы catalog.html
+(function () {
+  const filter = document.querySelector('.js__filter');
+  const buttonOpen = document.querySelector('.js__form-button-open');
+  const buttonClosed = document.querySelector('.js__form-button-closed');
+  const blockHiddenFilter = document.querySelectorAll('.js__block-hidden-filter');
+  const body = document.querySelector('.page-body');
+  const backgroundPopup = document.querySelector('.js__filter-background');
+
+  if (buttonOpen && filter && buttonClosed && blockHiddenFilter && body && backgroundPopup) {
+    filter.classList.add('js__filter--hidden');
+    buttonOpen.classList.add('js__form-button-open--active');
+
+    buttonOpen.addEventListener('click', () => {
+      filter.classList.remove('js__filter--hidden');
+      filter.classList.add('js__filter--popup');
+      buttonOpen.classList.remove('js__form-button-open--active');
+      buttonClosed.classList.add('js__form-button-closed--active');
+      body.classList.add('js__body--no-scroll');
+      backgroundPopup.classList.add('js__filter-background--popup');
+
+      blockHiddenFilter.forEach((block) => {
+        block.classList.add('js__block-hidden--active');
+      });
+    });
+
+    const deactivePopupFilter = () => {
+      filter.classList.add('js__filter--hidden');
+      filter.classList.remove('js__filter--popup');
+      buttonOpen.classList.add('js__form-button-open--active');
+      buttonClosed.classList.remove('js__form-button-closed--active');
+      body.classList.remove('js__body--no-scroll');
+      backgroundPopup.classList.remove('js__filter-background--popup');
+
+      blockHiddenFilter.forEach((block) => {
+        block.classList.remove('js__block-hidden--active');
+      });
+    };
+
+    buttonClosed.addEventListener('click', () => {
+      deactivePopupFilter();
+    });
+
+    if (screen.width <= 1023) {
+      filter.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        deactivePopupFilter();
+      });
+    }
+
+    document.addEventListener('keydown', (evt) => {
+      if (evt.key === 'Escape') {
+        deactivePopupFilter();
+      }
+    });
+
+    document.addEventListener('click', (evt) => {
+      if (evt.target === backgroundPopup) {
+        deactivePopupFilter();
+      }
     });
   }
 }());
